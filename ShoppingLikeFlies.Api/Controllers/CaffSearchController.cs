401656e4b6425b9a73dcd133be73ef7075d0cce8
Kerekes Áron Tamás <59024572+kerekesaron@users.noreply.github.com>
2022-12-04 17:53:09 +0100

    Create CaffSearchController.cs

---
+++ ShoppingLikeFlies.Api/Controllers/CaffSearchController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using ShoppingLikeFiles.DomainServices.DTOs;
+
+namespace ShoppingLikeFlies.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaffSearchController : ControllerBase
+    {
+        
+        private readonly IDataService dataService;
+        private readonly Serilog.ILogger logger;
+        private readonly IMapper mapper;
+
+        public CaffSearchController(ICaffService caffService, IDataService dataService, Serilog.ILogger logger, IMapper mapper)
+        {
+            this.dataService = dataService;
+            this.logger = logger;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<List<CaffAllResponse>> SearchAsync(CaffSearchDTO caffSearchDTO)
+        {
+            logger.Debug("Method {method} called with params: {username}", nameof(SearchAsync), caffSearchDTO);
+            var models = await dataService.SearchCaffAsync(caffSearchDTO);
+            var respone = mapper.Map<List<CaffAllResponse>>(models);
+            return respone;
+        }
+    }
+}
